fdf36352fa8576e278cb63925e1eca28fcf45e22
abdurrahman0605 <144532315+abdurrahman0605@users.noreply.github.com>
2025-09-03 16:01:12 +0530

    Replace Navbar.js with clean version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,46 @@
 ﻿import React from "react";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container-fluid">
-        <Link className="navbar-brand fw-bold" to="/">TutBuddy</Link>
-        <div className="collapse navbar-collapse">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          TutBuddy
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/quiz">Quiz</Link>
+              <Link className="nav-link" to="/quiz">
+                Quiz
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/worksheets">Worksheets</Link>
+              <Link className="nav-link" to="/worksheets">
+                Worksheets
+              </Link>
             </li>
           </ul>
         </div>
